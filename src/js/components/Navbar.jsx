8376eeea33dce9095bb2d30d14332ef289accc3c
Braian Dos Santos <102192666+Braianvladzgrid@users.noreply.github.com>
2026-03-02 22:24:51 +0000

    update

---
+++ src/js/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Inicio</li>
+        <li>Contacto</li>
+        <li>Acerca de</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
